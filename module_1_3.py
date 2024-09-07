b5f410fffd3289f66da988a81a9e15d0aad7040a
DarkIngr <Bartalex@mail.ru>
2024-09-07 13:43:57 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,9 @@
+name = 'Алексей'
+print('Name:', name)
+age = 49
+print('Age:', age)
+age = age + 1
+new_age = 50
+print('New Age:', age)
+is_student = new_age == age
+print('Is Student:', is_student)
---
+++ module_1_5.py
@@ -0,0 +1,9 @@
+immutable_var = (2024,'dragon','tree',)
+print('imutable var:', immutable_var)
+#immutable_var[0] = "2036"
+# Кортеж -неизменняемый тип данных. Не работает таким образом
+mutable_list = ['а','б','в',]
+mutable_list[0] = 'аист'
+mutable_list[1] = 'белка'
+mutable_list[2] = 'варан'
+print( 'mutable list:',mutable_list)
