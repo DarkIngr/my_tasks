4ca5380021e819854f8ff5dbdbcf170703669e1b
DarkIngr <Bartalex@mail.ru>
2024-09-09 06:19:33 +0300

    Add files via upload

---
+++ module_1_2.py
@@ -0,0 +1,5 @@
+home_work = 12
+hours_spent = 1.5
+course_name = 'Python'
+time_for = hours_spent / home_work
+print('Курс:',course_name,',всего задач:',home_work,',затрачено часов:',hours_spent,',среднее время выполнения',time_for,'часа.')
