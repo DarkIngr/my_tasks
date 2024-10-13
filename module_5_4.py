0c372eddf8b4094349ddeef7d307144e5ba8dcee
DarkIngr <Bartalex@mail.ru>
2024-10-13 15:22:41 +0300

    Add files via upload

--- module_5_4.py
+++ module_5_4.py
@@ -4,9 +4,9 @@ class House:
         cls.houses_history.append(args[0])
         return object.__new__(cls)
 
-    def __init__(self, name, floor):
+    def __init__(self, name, number_of_floors):
         self.name = name
-        self.floor = floor
+        self.number_of_floors = number_of_floors
 
     def __del__(self):
        print(f'{self.name} снесён, но он останется в истории')
