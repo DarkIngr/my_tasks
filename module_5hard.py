e38c0e49089f2706880c8bb39d6789dc6ff6879b
DarkIngr <Bartalex@mail.ru>
2024-10-16 17:28:59 +0300

    Add files via upload

--- module_5hard.py
+++ module_5hard.py
@@ -11,7 +11,7 @@ class Video:
     def __init__(self, title: str, duration: int, adult_mode: bool = False):
         self.title = title
         self.duration = duration
-        self.time_now = 1
+        self.time_now = 0
         self.adult_mode = adult_mode
 
     def __eq__(self, other):
