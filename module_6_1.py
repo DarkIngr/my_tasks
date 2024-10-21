1651aa4663c2df56a1aca0c39fe8b6b2d4ba2bce
DarkIngr <Bartalex@mail.ru>
2024-10-21 17:19:57 +0300

    Add files via upload

--- module_6_1.py
+++ module_6_1.py
@@ -31,9 +31,6 @@ class Flower(Plant):
 class Fruit(Plant):
     edible = True
 
-    def __init__(self, name):
-        super().__init__(name)
-
 a1 = Predator('Волк с Уолл-Стрит')
 a2 = Mammal('Хатико')
 p1 = Flower('Цветик семицветик')
