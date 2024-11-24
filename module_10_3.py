4d64714e8c4833acb5d484c17371c0c089f5b3de
DarkIngr <Bartalex@mail.ru>
2024-11-24 08:56:35 +0300

    Add files via upload

---
+++ module_10_3.py
@@ -0,0 +1,43 @@
+from random import randint
+from threading import Thread, Lock
+from time import sleep
+
+
+class Bank:
+    def __init__(self, balance: int = 0, lock: Lock = Lock()):
+        self.balance = balance
+        self.lock = lock
+
+    def deposit(self):
+        for i in range(100):
+            transfer = randint(50, 500)
+            self.balance += transfer
+            print(f'Пополнение: {transfer}. Баланс: {self.balance}')
+            if self.balance >= 500 and self.lock.locked():
+                self.lock.release()
+            sleep(0.001)
+
+    def take(self):
+        for i in range(100):
+            transfer = randint(50, 500)
+            print(f'Запрос на {transfer}')
+            if transfer <= self.balance:
+                self.balance -= transfer
+                print(f'Снятие: {transfer}. Баланс: {self.balance}')
+            else:
+                print('Запрос отклонён, недостаточно средств')
+                self.lock.acquire()
+            sleep(0.001)
+
+
+
+bk = Bank()
+th1 = Thread(target=bk.deposit)
+th2 = Thread(target=bk.take)
+
+th1.start()
+th2.start()
+th1.join()
+th2.join()
+
+print(f'Итоговый баланс: {bk.balance}')
